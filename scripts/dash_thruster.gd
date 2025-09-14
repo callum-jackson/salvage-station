6b700c1ea8724f0b5875e3df78129602f5d8cafb
Callum Jackson <honest.pollens-5j@icloud.com>
2025-09-15 09:09:24 +1000

    Create dash_thruster scene and begin testing.

--- scripts/dash_thruster.gd
+++ scripts/dash_thruster.gd
@@ -2,6 +2,9 @@ extends Area2D
 
 signal hit
 
+func _ready() -> void:
+	pass
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		$CollisionShape2D.set_deferred("disabled", true)
