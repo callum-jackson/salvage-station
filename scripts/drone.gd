6c1f0b76fbd0503bb65b1594e4791645fa0caa03
Callum <callum-jackson@users.noreply.github.com>
2025-11-24 09:57:04 +1100

    Tweak drone movement

--- scripts/drone.gd
+++ scripts/drone.gd
@@ -3,30 +3,18 @@ extends Area2D
 @export var speed = 40.0
 var path_follow = PathFollow2D
 var direction = 1
-var last_position = Vector2.ZERO
 
 func _ready():
 	position = Vector2(0, 0)
 	path_follow = get_parent()
-	if path_follow:
+	if path_follow and path_follow is PathFollow2D:
 		path_follow.loop = false
-	
-	rotation = 0.0
-	last_position = global_position
 
 func _physics_process(delta):
-	if path_follow:
+	if path_follow and path_follow is PathFollow2D:
 		path_follow.progress += speed * direction * delta
-
+		
 		if path_follow.progress_ratio >= 1.0:
 			direction = -1
 		elif path_follow.progress_ratio <= 0.0:
 			direction = 1
-
-		rotation = 0.0
-
-		var movement = global_position - last_position
-		if movement.length() > 0.1:
-			$AnimatedSprite2D.flip_h = movement.x < 0
-
-		last_position = global_position
