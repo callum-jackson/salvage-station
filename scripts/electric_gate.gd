91d5167c6853c97a7b44b4b77f96a576cab91b1d
Callum <callum-jackson@users.noreply.github.com>
2025-12-04 22:52:50 +1100

    Begin creating first level.

--- scripts/electric_gate.gd
+++ scripts/electric_gate.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
-@export var on_duration = 10.0
-@export var off_duration = 0.1
+@export var on_duration = 2.0
+@export var off_duration = 2.0
 @export var starts_active = true
 
 var is_active = true
@@ -24,9 +24,13 @@ func update_state() -> void:
 	if is_active:
 		$AnimatedSprite2D.play("active")
 		$CollisionShape2D.set_deferred("disabled", false)
+		if has_node("StaticBody2D/CollisionShape2D"):
+			$StaticBody2D/CollisionShape2D.set_deferred("disabled", false)
 	else:
 		$AnimatedSprite2D.play("inactive")
 		$CollisionShape2D.set_deferred("disabled", true)
+		if has_node("StaticBody2D/CollisionShape2D"):
+			$StaticBody2D/CollisionShape2D.set_deferred("disabled", true)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
