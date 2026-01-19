0762bec1387567dd2c14347190de2f788d8bad7c
Callum <callum-jackson@users.noreply.github.com>
2026-01-19 14:55:06 +1100

    Add thruster pickup sound effect

--- scripts/dash_thrusters.gd
+++ scripts/dash_thrusters.gd
@@ -7,4 +7,7 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		$CollisionShape2D.set_deferred("disabled", true)
 		game_state.has_thrusters = true
+		$AnimatedSprite2D.visible = false
+		$PickupSound.play()
+		await $PickupSound.finished
 		queue_free()
